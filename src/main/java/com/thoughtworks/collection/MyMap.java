cdd67075388157a38f6000678fa176e6cea0b46e
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-14 14:05:18 +0800

    [Collection] [Joseph] Development: Applied the handling on matching the integers to the letters

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -24,7 +24,9 @@ public class MyMap {
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return array.stream()
+                .map(integer -> letterList.get(integer - 1))
+                .collect(Collectors.toList());
     }
 
     public List<Integer> sortFromBig() {
